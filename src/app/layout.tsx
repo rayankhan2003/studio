cf59df002c66d0d7c962779a7a5f98ed81679f31
Kabir Khan <kabirkhancc5@gmail.com>
2025-12-27 23:55:32 +0000

    by lighthouse tool analytics,the website is not performingwell.it only s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
 
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from '@/hooks/use-auth';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const inter = Inter({
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -28,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
+    <html lang="en" className={inter.variable}>
+      <body className='antialiased flex flex-col min-h-screen'>
         <AuthProvider>
             <div className="flex flex-col min-h-screen">
               <Header />
